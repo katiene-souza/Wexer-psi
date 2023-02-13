0d19a90ec78c308f2af55fb8b8aa29faf79eda39
Marcos Azevedo <marcos.workspace@gmail.com>
2023-02-13 20:59:05 -0300

    chore: update extensions

--- src/main.tsx
+++ src/main.tsx
@@ -5,5 +5,5 @@ import App from '@/components/app'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
