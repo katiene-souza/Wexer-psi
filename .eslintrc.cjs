bd08f700c2b83b41e9fd08566a054392ecc7c396
Marcos Azevedo <marcos.workspace@gmail.com>
2023-02-28 20:51:56 -0300

    chore: update boilerplate

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,33 +1,25 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es2021": true,
-    "node": true
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
   },
-  "extends": [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-  ],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    "ecmaVersion": "latest",
-    "sourceType": "module"
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
-  "plugins": [
-    "react",
-    "@typescript-eslint"
-  ],
+  plugins: ['react', '@typescript-eslint'],
   rules: {
     'comma-dangle': 'off',
     'react/react-in-jsx-scope': 'off',
     '@typescript-eslint/no-use-before-define': ['error'],
     'jsx-quotes': ['error', 'prefer-double'],
-    'no-console': ['error', {allow: ['warn', 'error']}],
+    'no-console': ['error', { allow: ['warn', 'error'] }],
     camelcase: 'off',
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': [
@@ -35,16 +27,17 @@ module.exports = {
       {
         argsIgnorePattern: '^_',
         varsIgnorePattern: '^_',
-        caughtErrorsIgnorePattern: '^_',
-      },
+        caughtErrorsIgnorePattern: '^_'
+      }
     ],
     'no-useless-constructor': 'off',
     'no-redeclare': 'off',
     '@typescript-eslint/no-redeclare': [
       'error',
       {
-        ignoreDeclarationMerge: true,
-      },
-    ]
-  },
+        ignoreDeclarationMerge: true
+      }
+    ],
+    '@typescript-eslint/ban-types': 'off'
+  }
 }
